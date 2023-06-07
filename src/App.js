b3dc614b5e1cfc2e5777d97b8b5460a627b2ae44
unknown <timi.myllyaho@gmail.com>
2023-06-07 13:09:05 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+        </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [
+    {name: 'Leevi', age: 4 },
+    {name: 'Venla', age: 10 }, 
+  ]
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>   
+  )
+}
+
+export default App
